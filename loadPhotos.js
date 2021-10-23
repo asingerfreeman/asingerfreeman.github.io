3640093a11e530f7ba06f1d27bd7435cd8145c20
Ari Singer-Freeman <arisf@live.unc.edu>
2021-10-23 13:23:03 -0400

    retrying push

--- loadPhotos.js
+++ loadPhotos.js
@@ -1,18 +1,15 @@
-const container = document.querySelector('.container')
+const container = document.querySelector('.photo-holder')
 
 let URLs = [['img/snow/snow-1.jpg', 'img/snow/snow-2.jpg', 'img/snow/snow-3.jpg', 'img/snow/snow-4.jpg'],['img/desert/desert-1.jpg', 'img/desert/desert-2.jpg', 'img/desert/desert-3.jpg', 'img/desert/desert-4.jpg']]
 
 function loadImages(numClasses =1){
     let i = 0
-    console.log("reached")
     while (i < numClasses){
         let j = 0
-        console.log("1")
         while (j < URLs.length){
-            console.log("2")
-            const imgDiv = document.createElement('img')
-            imgDiv.src= URLs[i][j]
-            container.appendChild(imgDiv)
+            const img = document.createElement('img')
+            img.src = URLs[i][j]
+            container.appendChild(img)
             j++
         }
         i++
