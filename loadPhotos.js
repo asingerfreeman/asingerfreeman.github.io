5cbc7c04e6993f8100fe384ffb97a7e389dd1861
Ari Singer-Freeman <arisf@live.unc.edu>
2021-10-23 12:43:08 -0400

    reverted

--- loadPhotos.js
+++ loadPhotos.js
@@ -1,15 +1,18 @@
-const container = document.querySelector('.photo-holder')
+const container = document.querySelector('.container')
 
 let URLs = [['img/snow/snow-1.jpg', 'img/snow/snow-2.jpg', 'img/snow/snow-3.jpg', 'img/snow/snow-4.jpg'],['img/desert/desert-1.jpg', 'img/desert/desert-2.jpg', 'img/desert/desert-3.jpg', 'img/desert/desert-4.jpg']]
 
 function loadImages(numClasses =1){
     let i = 0
+    console.log("reached")
     while (i < numClasses){
         let j = 0
+        console.log("1")
         while (j < URLs.length){
-            const img = document.createElement('img')
-            img.src = URLs[i][j]
-            container.appendChild(img)
+            console.log("2")
+            const imgDiv = document.createElement('img')
+            imgDiv.src= URLs[i][j]
+            container.appendChild(imgDiv)
             j++
         }
         i++
